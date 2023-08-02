5b322a4021011dbb18396df7803daf9b932bb1a7
Andrew D Smith <andrewds@usc.edu>
2023-08-02 13:02:20 -0700

    Removing indentation of namespace

--- bamxx.hpp
+++ bamxx.hpp
@@ -33,103 +33,106 @@
 
 namespace bamxx {
 
-  struct bam_rec {
-    bam_rec() = default;
+struct bam_rec {
+  bam_rec() = default;
 
-    bam_rec(const bam_rec &other): b{bam_copy1(bam_init1(), other.b)} {}
+  bam_rec(const bam_rec &other): b{bam_copy1(bam_init1(), other.b)} {}
 
-    auto operator=(bam_rec rhs) -> bam_rec & {
-      std::swap(b, rhs.b);
-      return *this;
-    }
+  auto operator=(bam_rec rhs) -> bam_rec & {
+    std::swap(b, rhs.b);
+    return *this;
+  }
 
-    ~bam_rec() {
-      if (b != nullptr) bam_destroy1(b);
-    }
+  ~bam_rec() {
+    if (b != nullptr) bam_destroy1(b);
+  }
 
-    bam1_t *b{};
-  };
+  bam1_t *b{};
+};
 
-  struct bam_in {
-    explicit bam_in(const std::string &fn): f{hts_open(fn.c_str(), "r")} {}
+struct bam_in {
+  explicit bam_in(const std::string &fn): f{hts_open(fn.c_str(), "r")} {}
 
-    ~bam_in() {
-      if (f != nullptr) hts_close(f);
-    }
+  ~bam_in() {
+    if (f != nullptr) hts_close(f);
+  }
 
-    operator bool() const { return f != nullptr; }
+  operator bool() const { return f != nullptr; }
 
-    template<typename T> auto read(T &h, bam_rec &b) -> bool {
-      const int x = sam_read1(f, h.h, b.b);  // -1 on EOF; args non-const
-      if (x < -1) throw std::runtime_error("failed reading bam record");
-      return x >= 0;
-    }
+  template<typename T> auto read(T &h, bam_rec &b) -> bool {
+    const int x = sam_read1(f, h.h, b.b);  // -1 on EOF; args non-const
+    // ADS: (todo) get rid of exception
+    if (x < -1) throw std::runtime_error("failed reading bam record");
+    return x >= 0;
+  }
 
-    auto is_mapped_reads_file() const -> bool {
-      const htsFormat *fmt = hts_get_format(f);
-      return fmt->category == sequence_data &&
-        (fmt->format == bam || fmt->format == sam);
-    }
+  auto is_mapped_reads_file() const -> bool {
+    const htsFormat *fmt = hts_get_format(f);
+    return fmt->category == sequence_data &&
+           (fmt->format == bam || fmt->format == sam);
+  }
 
-    samFile *f{};
-  };
+  samFile *f{};
+};
 
-  struct bam_header {
-    bam_header() = default;
+struct bam_header {
+  bam_header() = default;
 
-    bam_header(const bam_header &rhs): h{bam_hdr_dup(rhs.h)} {}
+  bam_header(const bam_header &rhs): h{bam_hdr_dup(rhs.h)} {}
 
-    explicit bam_header(bam_in &in): h{sam_hdr_read(in.f)} {}
+  explicit bam_header(bam_in &in): h{sam_hdr_read(in.f)} {}
 
-    ~bam_header() {
-      if (h != nullptr) bam_hdr_destroy(h);
-    }
+  ~bam_header() {
+    if (h != nullptr) bam_hdr_destroy(h);
+  }
 
-    operator bool() const { return h != nullptr; }
+  operator bool() const { return h != nullptr; }
 
-    auto add_pg_line(const std::string &cmd, const std::string &id,
-                     const std::string &vn) -> bool {
-      return sam_hdr_add_line(h, "PG", "ID", id.c_str(), "VN", vn.c_str(), "CL",
-                              cmd.c_str(), nullptr) == 0;
-    }
+  auto add_pg_line(const std::string &cmd, const std::string &id,
+                   const std::string &vn) -> bool {
+    return sam_hdr_add_line(h, "PG", "ID", id.c_str(), "VN", vn.c_str(), "CL",
+                            cmd.c_str(), nullptr) == 0;
+  }
 
-    auto tostring() const -> std::string { return sam_hdr_str(h); }
+  auto tostring() const -> std::string { return sam_hdr_str(h); }
 
-    sam_hdr_t *h{};
-  };
+  sam_hdr_t *h{};
+};
 
-  struct bam_out {
-    explicit bam_out(const std::string &fn, const bool fmt = false)
+struct bam_out {
+  explicit bam_out(const std::string &fn, const bool fmt = false)
       : f{hts_open(fn.c_str(), fmt ? "bw" : "w")} {}
 
-    ~bam_out() {
-      if (f != nullptr) hts_close(f);
-    }
+  ~bam_out() {
+    if (f != nullptr) hts_close(f);
+  }
 
-    operator bool() const { return f != nullptr; }
+  operator bool() const { return f != nullptr; }
 
-    auto write(const bam_header &h, const bam_rec &b) -> bool {
-      return sam_write1(f, h.h, b.b) >= 0;
-    }
+  auto write(const bam_header &h, const bam_rec &b) -> bool {
+    return sam_write1(f, h.h, b.b) >= 0;
+  }
 
-    auto write(const bam_header &h) -> bool { return sam_hdr_write(f, h.h) == 0; }
+  auto write(const bam_header &h) -> bool { return sam_hdr_write(f, h.h) == 0; }
 
-    htsFile *f{};
-  };
+  htsFile *f{};
+};
 
-  struct bam_tpool {
-    explicit bam_tpool(const int n_threads)
+struct bam_tpool {
+  explicit bam_tpool(const int n_threads)
       : tpool{hts_tpool_init(n_threads), 0} {}
 
-    ~bam_tpool() { hts_tpool_destroy(tpool.pool); }
+  ~bam_tpool() { hts_tpool_destroy(tpool.pool); }
 
-    template<class T> auto set_io(const T &bam_file) -> void {
-      const int ret = hts_set_thread_pool(bam_file.f, &tpool);
-      if (ret < 0) throw std::runtime_error("failed to set thread pool");
-    }
+  template<class T> auto set_io(const T &bam_file) -> void {
+    const int ret = hts_set_thread_pool(bam_file.f, &tpool);
+    // ADS: (todo) get rid of exception
+    if (ret < 0) throw std::runtime_error("failed to set thread pool");
+  }
 
-    htsThreadPool tpool{};
-  };
+  htsThreadPool tpool{};
 };
+
+};  // namespace bamxx
 
 #endif
